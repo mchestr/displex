99d96345716cf8ff590af8f95df41b7c78ec4056
Mike Chester <mike@chester.io>
2023-09-29 22:55:54 -0700

    remove testing code

--- src/bot/discord/commands/ping.rs
+++ src/bot/discord/commands/ping.rs
@@ -9,19 +9,6 @@ use serenity::{
 
 #[command]
 pub async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    tracing::info!("testing got here!");
     msg.reply(ctx, "Pong!").await?;
-
-    let result = ctx
-        .http
-        .remove_member_role(
-            434573248206733312,
-            229726290972901377,
-            1107886515360632902,
-            Some("removed for testing"),
-        )
-        .await;
-    tracing::info!("Result: {:?}", result);
-
     Ok(())
 }
