35ee57622b5338dc58d05f3c95fb9463f078663f
Mike Chester <mike@chester.io>
2023-10-09 11:54:52 -0700

    fmt

--- src/metrics.rs
+++ src/metrics.rs
@@ -49,6 +49,10 @@ impl Metrics {
 
 pub fn new_registry(metrics: &Metrics) -> Registry {
     let mut registry = Registry::default();
-    registry.register("displex_counters", "counter metrics", metrics.counters.clone());
+    registry.register(
+        "displex_counters",
+        "counter metrics",
+        metrics.counters.clone(),
+    );
     registry
 }
