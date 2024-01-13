8f5c634972a413dd374eadba105b39a57906d37b
Mike Chester <mike@chester.io>
2024-01-13 11:49:18 -0800

    yoink tailadmin

---
+++ web/src/App.tsx
@@ -0,0 +1,54 @@
+import { Suspense, lazy, useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import ECommerce from './pages/Dashboard/ECommerce';
+import SignIn from './pages/Authentication/SignIn';
+import SignUp from './pages/Authentication/SignUp';
+import Loader from './common/Loader';
+import routes from './routes';
+
+const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
+
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 500);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        containerClassName="overflow-auto"
+      />
+      <Routes>
+        <Route path="/auth/signin" element={<SignIn />} />
+        <Route path="/auth/signup" element={<SignUp />} />
+        <Route element={<DefaultLayout />}>
+          <Route index element={<ECommerce />} />
+          {routes.map((routes, index) => {
+            const { path, component: Component } = routes;
+            return (
+              <Route
+                key={index}
+                path={path}
+                element={
+                  <Suspense fallback={<Loader />}>
+                    <Component />
+                  </Suspense>
+                }
+              />
+            );
+          })}
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,65 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import './satoshi.css';
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/root";
-import ErrorPage from "./error-page";
-import Admin from "./routes/admin";
-import UserSummary from "./routes/user-summary";
-import Recap from "./routes/recap";
-
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
-
-const client = new ApolloClient({
-  uri: "http://localhost:8080/gql",
-  cache: new InMemoryCache(),
-});
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/admin",
-        element: <Admin />,
-        children: [
-          {
-            path: "users/:id",
-            element: <UserSummary />,
-          },
-        ],
-      },
-      {
-        path: "/recap/:year",
-        element: <Recap />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <ApolloProvider client={client}>
-        <CssBaseline />
-        <RouterProvider router={router} />
-      </ApolloProvider>
-    </ThemeProvider>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
