434e662bc09f5da600d239013f3aa5580d40a1e9
Mike Chester <mike@chester.io>
2024-01-12 23:18:50 -0800

    fix error page

--- web/src/error-page.tsx
+++ web/src/error-page.tsx
@@ -1,16 +1,35 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 
-export default function ErrorPage() {
+const ErrorPage: React.FC = () => {
+  // you don't need to explicitly set error to `unknown`
   const error = useRouteError();
+  let errorMessage: string;
+
+  if (isRouteErrorResponse(error)) {
+    // error is type `ErrorResponse`
+    errorMessage = error.data.message || error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === "string") {
+    errorMessage = error;
+  } else {
+    console.error(error);
+    errorMessage = "Unknown error";
+  }
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
+    <div
+      id="error-page"
+      className="flex flex-col gap-8 justify-center items-center h-screen"
+    >
+      <h1 className="text-4xl font-bold">Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
+      <p className="text-slate-400">
+        <i>{errorMessage}</i>
       </p>
     </div>
   );
-}
+};
+
+export default ErrorPage;
